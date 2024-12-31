d4d2ba17dda796da80f41d63d88da6dfa0e72824
qnnp <58357109+qnnp-me@users.noreply.github.com>
2024-12-31 22:57:17 +0800

    refactor(wegar-basic):重构 Phinx 迁移和种子功能
    - 新增 PhinxHelper 类，将 Phinx 相关功能封装到单独的 helper 中- 从 Phinx 初始化文件中移除冗余代码，提高代码可读性和可维护性
    - 优化了迁移和种子过程的错误处理和日志输出

---
+++ src/helper/PhinxHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wegar\Basic\helper;
+
+use Phinx\Console\PhinxApplication;
+use Phinx\Wrapper\TextWrapper;
+use support\helper\CommandHelper;
+
+class PhinxHelper
+{
+  static function load(string $config_path): void
+  {
+    $command_helper = new CommandHelper();
+    $command_helper->notice("Loading Phinx -> $config_path");
+    $app = new PhinxApplication();
+    $wrap = new TextWrapper($app);
+    $wrap->setOption('configuration', $config_path);
+    $config = include $config_path;
+
+    $seed_path = $config['paths']['seeds'];
+    $seeds = [];
+    if (is_dir($seed_path)) {
+      foreach (scandir($seed_path) as $file) {
+        if (str_ends_with($file, '.php')) {
+          $class_name = substr($file, 0, -4);
+          if (class_exists($class_name)) {
+            $seeds[] = "$class_name";
+          }
+        }
+      }
+    }
+
+    $migrate_result = $wrap->getMigrate();
+    $has_error = str_contains($migrate_result, 'Exception:');
+    if ($has_error || str_contains($migrate_result, ' == ')) {
+      $command_helper->notice('Phinx Migrate');
+      $command_helper->{$has_error ? 'error' : 'info'}(explode("\n", $migrate_result));
+    }
+
+    $seed_result = $wrap->getSeed(seed: $seeds);
+    $has_error = str_contains($seed_result, 'Exception:');
+    if ($has_error || str_contains($seed_result, ' == ')) {
+      $command_helper->notice('Phinx Seed');
+      $command_helper->{$has_error ? 'error' : 'info'}(explode("\n", $seed_result));
+    }
+  }
+}
--- src/init/Phinx.php
+++ src/init/Phinx.php
@@ -2,10 +2,8 @@
 
 namespace Wegar\Basic\init;
 
-use Phinx\Console\PhinxApplication;
-use Phinx\Wrapper\TextWrapper;
 use Wegar\Basic\abstract\InitAbstract;
-use Wegar\Basic\helper\CommandHelper;
+use Wegar\Basic\helper\PhinxHelper;
 
 class Phinx extends InitAbstract
 {
@@ -15,37 +13,6 @@ class Phinx extends InitAbstract
     if (!file_exists(base_path('phinx.php'))) {
       return;
     }
-    $command_helper = new CommandHelper();
-    $app = new PhinxApplication();
-    $wrap = new TextWrapper($app);
-    $wrap->setOption('configuration', base_path('phinx.php'));
-    $config = include base_path('phinx.php');
-
-    $seed_path = $config['paths']['seeds'];
-    $seeds = [];
-    if (is_dir($seed_path)) {
-      foreach (scandir($seed_path) as $file) {
-        if (str_ends_with($file, '.php')) {
-          $class_name = substr($file, 0, -4);
-          if (class_exists($class_name)) {
-            $seeds[] = "\\$class_name";
-          }
-        }
-      }
-    }
-
-    $migrate_result = $wrap->getMigrate();
-    $has_error = str_contains($migrate_result, 'Exception:');
-    if ($has_error || str_contains($migrate_result, ' == ')) {
-      $command_helper->notice('Phinx Migrate');
-      $command_helper->{$has_error ? 'error' : 'info'}(explode("\n", $migrate_result));
-    }
-
-    $seed_result = $wrap->getSeed(seed: $seeds);
-    $has_error = str_contains($seed_result, 'Exception:');
-    if ($has_error || str_contains($seed_result, ' == ')) {
-      $command_helper->notice('Phinx Seed');
-      $command_helper->{$has_error ? 'error' : 'info'}(explode("\n", $seed_result));
-    }
+    PhinxHelper::load(base_path('phinx.php'));
   }
 }
